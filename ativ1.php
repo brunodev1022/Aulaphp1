791fbb4ceb0582fc56a42a608072667e1554d899
brunodev1022 <brunocampos1022@gmail.com>
2026-04-07 11:02:51 -0300

    Atividade1 php - Bruno Campos

---
+++ ativ1.php
@@ -0,0 +1,30 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Atividade1</title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+    <div class="container">
+        <img src="site_senac.png" alt="Logo Senac"
+        class="logo-senac">
+        <h2>Cadastro</h2>
+    
+<form method="POST"
+    action="resposta.php">
+
+    <input type="text"
+        name="nome"
+        placeholder="Seu Nome" required>
+    <br>
+    <input type="number"
+        name="idade"
+        placeholder="Sua Idade" required>
+    <br>
+    <button type="submit">
+        Enviar
+    </button>
+    </div>
+</form>  
+</body>
+</html>
---
+++ resposta.php
@@ -0,0 +1,7 @@
+<?php
+        $nome = $_POST["nome"];
+        $idade = $_POST["idade"];
+    echo "Olá, $nome !";
+    echo "<br>";
+    echo "Voce tem $idade anos.";
+    ?>  
